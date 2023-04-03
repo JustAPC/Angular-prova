cfd2359049fdb527714b6dd64038dac5916928a4
JustAPC <andreapontillo.dev@gmail.com>
2023-04-03 17:40:01 +0200

    card iteration for array data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+
+export class AppComponent implements OnInit {
+  cuisines: any;
+  restaurants: any;
+
+  constructor(private service:PostService) {}
+
+  ngOnInit() {
+      this.service.getCuisinesList()
+        .subscribe(response => {
+          this.cuisines = response;
+          console.log(this.cuisines);
+        });
+
+      this.service.getRestaurantsList()
+        .subscribe(response => {
+          this.restaurants = response;
+          console.log(this.restaurants);
+        });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { CardComponent } from './components/card/card.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent
   ],
   imports: [
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
     BrowserModule,
+    HttpClientModule,
+    MatSlideToggleModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
@@ -18,3 +32,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export class SelectErrorStateMatcherExample {
+  selected = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+
+  selectFormControl = new FormControl('valid', [Validators.required, Validators.pattern('valid')]);
+
+  nativeSelectFormControl = new FormControl('valid', [
+    Validators.required,
+    Validators.pattern('valid'),
+  ]);
+
+  matcher = new MyErrorStateMatcher();
+}
